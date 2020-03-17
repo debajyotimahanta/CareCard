9cdc768d126bed827f5efe3a37c364f16ddb709d
debamahanta <debajyoti.mahanta@gmail.com>
2020-03-17 15:10:23 -0700

    Remove contacts as its one to one mapping

---
+++ src/test/java/com/coronacarecard/service/impl/BusinessServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.coronacarecard.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BusinessServiceImplTest {
+
+    @Test
+    public void create() {
+    }
+}
