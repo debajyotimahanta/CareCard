39e931e2c0efb091543bd49c7a425dd0208b83fd
Arun Mukherjee <aruns.devacc@gmail.com>
2020-04-04 15:45:38 -0500

    Added tests for handling exception

--- src/test/java/com/coronacarecard/config/ApiExceptionControllerAdviceTest.java
+++ src/test/java/com/coronacarecard/config/ApiExceptionControllerAdviceTest.java
@@ -1,28 +1,46 @@
 package com.coronacarecard.config;
 
-import com.coronacarecard.exceptions.*;
+import com.coronacarecard.controller.BusinessController;
+import com.coronacarecard.exceptions.BusinessAlreadyClaimedException;
+import com.coronacarecard.exceptions.BusinessClaimException;
+import com.coronacarecard.exceptions.InternalException;
+import com.coronacarecard.exceptions.PaymentServiceException;
+import com.coronacarecard.service.BusinessService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApiExceptionControllerAdviceTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    BusinessService businessService;
+
+    @Autowired
+    BusinessController businessController;
 
     @Autowired
     ApiExceptionControllerAdvice target;
 
-    final String INTERNAL_SERVER_ERROR    = "Internal Server Error";
-    final String PAYMENT_SERVICE_ERROR       = "Payment service could not process the request";
-    final String VENDOR_NOT_REGISTERED_WITH_PAYMENT_SERVICE         = "Vendor is not registered with Payment Service";
-    final String UNABLE_TO_CLAIM_BUSINESS = "Unable to import business. Error retrieving details from Payment Service";
+    final String INTERNAL_SERVER_ERROR                      = "Internal Server Error";
+    final String PAYMENT_SERVICE_ERROR                      = "Payment service could not process the request";
+    final String VENDOR_NOT_REGISTERED_WITH_PAYMENT_SERVICE = "Vendor is not registered with Payment Service";
+    final String UNABLE_TO_CLAIM_BUSINESS                   = "Unable to import business. Error retrieving details from Payment Service";
+    final String BUSINESS_NOT_FOUND                         = "Unable to find the business";
 
     void setUp() throws Exception {
 
