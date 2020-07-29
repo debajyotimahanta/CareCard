56d0b32ed99e3876ee037e101a63b489b8724c1c
Quintin Tucker <36942351+QuintinTucker@users.noreply.github.com>
2020-07-29 11:55:35 -0700

    Create Nominator.java
    Adding Nominator entity

---
+++ src/main/java/com/coronacarecard/dao/entity/Nominator.java
@@ -0,0 +1,20 @@
+package com.coronacarecard.dao.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@lombok.Builder(toBuilder = true)
+@lombok.NoArgsConstructor
+@lombok.AllArgsConstructor
+@lombok.Getter
+@lombok.Setter
+@Entity
+public class Nominator {
+@Id
+    @GeneratedValue(generator = "uuid2")
+    @Column(name = "id", columnDefinition = "binary(16)")
+    private UUID id;
+    private String email;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Business business;
+}
